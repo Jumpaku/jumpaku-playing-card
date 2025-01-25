5952b9c21ec4257d34bebdc50fc3a33b12d62b33
Jumpaku <Jumpaku@users.noreply.github.com>
2025-01-26 07:36:48 +0900

    feat(infra): add migrations

--- backend/infra/migration/00001_create_example.up.sql
+++ backend/infra/migration/00001_create_example.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE "Example"
 (
     "example_id"  TEXT      NOT NULL,
     "name"        TEXT      NOT NULL,
+    "content"     TEXT      NOT NULL,
     "create_time" TIMESTAMP WITH TIME ZONE NOT NULL,
     "update_time" TIMESTAMP WITH TIME ZONE NOT NULL,
-    "content"     TEXT      NOT NULL,
     PRIMARY KEY ("example_id")
 );
