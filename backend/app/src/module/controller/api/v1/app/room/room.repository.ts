91e3d074f7b8e67a6c18017757e70a74b7d50a62
Jumpaku <Jumpaku@users.noreply.github.com>
2025-04-20 22:59:29 +0900

    refactor(room): refactor room

--- backend/app/src/module/controller/api/v1/app/room/room.repository.ts
+++ backend/app/src/module/controller/api/v1/app/room/room.repository.ts
@@ -1,15 +1,96 @@
 import {Injectable} from "@nestjs/common";
-import {User$} from "../../../../../../gen/pg/dao/dao_User";
-import {PgClient} from "../../../../../global/postgres.provider";
+import {PgClient, selectAll} from "../../../../../global/postgres.provider";
+import {Room$} from "../../../../../../gen/pg/dao/dao_Room";
+import {RoomSeat$} from "../../../../../../gen/pg/dao/dao_RoomSeat";
+import {RoomMember$} from "../../../../../../gen/pg/dao/dao_RoomMember";
+import {panic} from "../../../../../../lib/panic";
 
 @Injectable()
 export class RoomRepository {
-    async create(tx: PgClient, userId: string, displayName: string, t: Date): Promise<void> {
-        await User$.insert(tx, {
-            user_id: userId,
-            display_name: displayName,
+    async create(tx: PgClient, roomId: string, roomName: string, t: Date): Promise<void> {
+        await Room$.insert(tx, {
+            room_id: roomId,
+            room_name: roomName,
             create_time: t,
             update_time: t,
+        });
+    }
+
+    async exists(tx: PgClient, roomId: string): Promise<boolean> {
+        const room = await Room$.find(tx, {room_id: roomId});
+        return room != null;
+    }
+
+    async find(tx: PgClient, roomId: string): Promise<{
+        room: Room$,
+        seatList: RoomSeat$[],
+        memberList: RoomMember$[],
+    } | null> {
+        const room = await Room$.find(tx, {room_id: roomId});
+        if (room == null) {
+            return null;
+        }
+        const seatList = await selectAll<RoomSeat$>(tx,
+            `SELECT *
+             FROM "RoomSeat"
+             WHERE "room_id" = $1
+             ORDER BY "room_seat_id"`,
+            [roomId],
+        );
+        const memberList = await selectAll<RoomMember$>(tx,
+            `SELECT *
+             FROM "RoomMember"
+             WHERE "room_id" = $1
+             ORDER BY "room_member_id"`,
+            [roomId],
+        );
+        return {room, seatList, memberList};
+    }
+
+    async createSeat(tx: PgClient, roomId: string, seatId: string, seatName: string, memberId: string | null, t: Date): Promise<void> {
+        await RoomSeat$.insert(tx, {
+            room_seat_id: seatId,
+            room_id: roomId,
+            room_seat_name: seatName,
+            room_member_id: memberId,
+            create_time: t,
+            update_time: t,
+        });
+    }
+
+    async existsSeat(tx: PgClient, roomId: string, seatId: string): Promise<boolean> {
+        const s = await RoomSeat$.find(tx, {room_seat_id: seatId});
+        return !(s == null || s.room_id !== roomId);
+    }
+
+    async updateSeatMember(tx: PgClient, roomId: string, seatId: string, memberId: string | null, t: Date): Promise<void> {
+        const s = await RoomSeat$.find(tx, {room_seat_id: seatId});
+        if (s == null) {
+            panic(`Seat not found: ${seatId}`);
+        }
+        if (s.room_id !== roomId) {
+            panic(`Seat not in room: ${roomId} ${seatId}`);
+        }
+        s.room_member_id = memberId;
+        s.update_time = t;
+        await RoomSeat$.upsert(tx, s);
+    }
+
+    async createMember(tx: PgClient, roomId: string, userId: string, memberId: string, roleId: string, t: Date): Promise<void> {
+        await RoomMember$.insert(tx, {
+            user_id: userId,
+            room_id: roomId,
+            room_member_id: memberId,
+            role_id: roleId,
+            create_time: t,
+            update_time: t,
+        });
+    }
+
+    async findMemberByUserId(tx: PgClient, roomId: string, userId: string): Promise<RoomMember$ | null> {
+        return await RoomMember$.findByUq_RoomMember_RoomUser(tx, {
+            user_id: userId,
+            room_id: roomId,
         });
     }
 }
